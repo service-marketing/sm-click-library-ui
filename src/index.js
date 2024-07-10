05191d844ac8c16c9c767aedd101717bc10600f9
LeoA98 <leonardo.araujo-silva@servicemarketing-tecnologia.com>
2024-07-10 13:38:23 -0300

    fix render component modal

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import primarySelect from '~/components/selects/primary_select/primary_select.vue'
+import simpleModal from "~/components/modals/simple_modal/simple_modal.vue"
+
 export {
-    primarySelect
+    primarySelect, simpleModal
 }
 
 function install(Vue) {
     Vue.component('primarySelect', primarySelect)
+    Vue.component('simpleModal', simpleModal)
 }
 
 export default { install: install }
