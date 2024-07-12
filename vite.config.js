0cf6eed9ec00ece3621882078dbcc466fdd94fbe
LeoA98 <leonardo.araujo-silva@servicemarketing-tecnologia.com>
2024-07-12 17:37:06 -0300

    test css

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import cssOnly from 'rollup-plugin-css-only';
+// import cssOnly from 'rollup-plugin-css-only';
 import path from 'path'
 
 // https://vitejs.dev/config/
@@ -22,9 +22,9 @@ export default defineConfig({
     }
   },
   plugins: [vue(),
-  cssOnly({
-    output: 'styleSmClick.css'
-  })
+  // cssOnly({
+  //   output: 'styleSmClick.css'
+  // })
   ],
   resolve: {
     alias: {
