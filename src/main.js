4408a609c13dd3416fd585442ec482a38a055c34
LeoA98 <leonardo.araujo-silva@servicemarketing-tecnologia.com>
2024-07-15 12:20:27 -0300

    css test

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import '~/styleSmClick.css'
+import '~/styles/styleSmClick.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import cssOnly from 'rollup-plugin-css-only';
 import path from 'path'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -28,6 +30,14 @@ export default defineConfig({
   resolve: {
     alias: {
       "~/": path.resolve(__dirname, "src") + '/',
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss,
+        autoprefixer,
+      ],
     },
   },
 })
