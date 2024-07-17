4afff81dad8034956ff64a95d4b6f2fcc60ffda5
LeoA98 <leonardo.araujo-silva@servicemarketing-tecnologia.com>
2024-07-17 18:43:46 -0300

    export component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import primarySelect from '~/components/selects/primary_select/primary_select.vue'
 import simpleModal from "~/components/modals/simple_modal/simple_modal.vue"
+import simpleCard from "~/components/cards/simple_card/simple_card.vue";
 
 function install(Vue) {
     Vue.component('primarySelect', primarySelect)
     Vue.component('simpleModal', simpleModal)
+    Vue.component('simpleCard', simpleCard)
 }
 
 export default {
     install,
     primarySelect,
-    simpleModal
+    simpleModal,
+    simpleCard
 }
