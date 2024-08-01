69ccf65dc8196b7c7fe380485094c10d53a7e300
JoaoDevSM <joao.souto-santos@servicemarketing-tecnologia.com>
2024-08-01 10:06:10 -0300

    update

--- src/components/selects/instance_select/index.vue
+++ src/components/selects/instance_select/index.vue
@@ -204,7 +204,7 @@ function formatTelephone(number) {
             class="rounded-b-lg overflow-y-auto max-h-[200px] absolute top-[48px] w-full z-20 text-sm shadow shadow-black dark:shadow-gray-400 bg-base-300 p-2">
             <ul class="gap-2 flex flex-col">
                 <li v-if="instances.length > 0" v-for="inst, index in instances"
-                    class=" select-none cursor-pointer w-full">
+                    class="select-none cursor-pointer w-full">
                     <button
                         :class="selectedInstance && selectedInstance.id === inst.id ? 'bg-base-100' : 'bg-base-200 hover:bg-base-100'"
                         @click="selectedInstance = inst, open = false, functionEmit(inst)"
@@ -230,12 +230,12 @@ function formatTelephone(number) {
                                         d="M8.217 5.243C9.145 3.988 10.171 3 11.483 3 13.96 3 16 6.153 16.001 9.907c0 2.29-.986 3.725-2.757 3.725-1.543 0-2.395-.866-3.924-3.424l-.667-1.123-.118-.197a54.944 54.944 0 0 0-.53-.877l-1.178 2.08c-1.673 2.925-2.615 3.541-3.923 3.541C1.086 13.632 0 12.217 0 9.973 0 6.388 1.995 3 4.598 3c.319 0 .625.039.924.122.31.086.611.22.913.407.577.359 1.154.915 1.782 1.714Zm1.516 2.224c-.252-.41-.494-.787-.727-1.133L9 6.326c.845-1.305 1.543-1.954 2.372-1.954 1.723 0 3.102 2.537 3.102 5.653 0 1.188-.39 1.877-1.195 1.877-.773 0-1.142-.51-2.61-2.87l-.937-1.565ZM4.846 4.756c.725.1 1.385.634 2.34 2.001A212.13 212.13 0 0 0 5.551 9.3c-1.357 2.126-1.826 2.603-2.581 2.603-.777 0-1.24-.682-1.24-1.9 0-2.602 1.298-5.264 2.846-5.264.091 0 .181.006.27.018Z" />
                                 </svg>
                             </div>
-                            <Popper class="dark:popper-light popper-dark" :hover="true" placement="left">
+                            <Popper class="dark:popper-light popper-dark" :hover="true" placement="right">
                                 <template #content>
-                                    <span>{{ type && inst.type !== type ? 'Esse tipo de instância não é permitido nessa aplicação.' : webhooks === true ? 'O atendimento não está habilitado para essa instância' : 'Essa aplicação é somente para instâncias sem atendimento habilitado.' }}</span>
+                                    <span>{{ type && inst.type !== type ? 'Não é possível utilizar esse tipo de Instância neste recurso' : webhooks === true ? 'Essa instância está com o Atendimento desabilitado' : 'Essa aplicação é somente para instâncias sem atendimento habilitado.' }}</span>
                                 </template>
                                 <svg v-if="(type && inst.type !== type) || (webhooks !== undefined && webhooks !== inst.webhooks)"
-                                    class=" w-6 h-6 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                    class="w-6 h-6 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                     width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                     <path fill-rule="evenodd"
                                         d="M8 10V7a4 4 0 1 1 8 0v3h1a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h1Zm2-3a2 2 0 1 1 4 0v3h-4V7Zm2 6a1 1 0 0 1 1 1v3a1 1 0 1 1-2 0v-3a1 1 0 0 1 1-1Z"
