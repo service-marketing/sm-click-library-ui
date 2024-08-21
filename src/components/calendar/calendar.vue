bcac7ddea817fb7e463b73964a6c985cbf1b7060
JoaoDevSM <133679372+JoaoDevSM@users.noreply.github.com>
2024-08-21 09:24:51 -0300

    update

--- src/components/calendar/calendar.vue
+++ src/components/calendar/calendar.vue
@@ -36,9 +36,13 @@
                 <footer class="events-container">
                     <div v-for="event in day.events" :key="event.title" class="event" :class="event.tag">
                         <main class="event-main">
-                            <Popper placement="top" class="popper" :hover="true" :content="event.hours">
-                                <svg class="event-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                                    <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z" clip-rule="evenodd" />
+                            <Popper placement="top" class="dark:popper-light popper-dark" :hover="true"
+                                :content="event.hours">
+                                <svg class="event-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                    fill="currentColor" viewBox="0 0 24 24">
+                                    <path fill-rule="evenodd"
+                                        d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z"
+                                        clip-rule="evenodd" />
                                 </svg>
                             </Popper>
                             <p class="event-title">{{ event.title }}</p>
