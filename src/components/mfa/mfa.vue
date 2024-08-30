266df38e3f0ffffe321a4abafc6a245d089cf4b4
LeoA98 <leonardo.araujo-silva@servicemarketing-tecnologia.com>
2024-08-30 15:34:43 -0300

    v.0 MFA

--- src/components/mfa/mfa.vue
+++ src/components/mfa/mfa.vue
@@ -21,6 +21,16 @@
       </div>
     </div>
   </section>
+
+  <div class="w-[30%]">
+    <Card
+      :content="{
+        title: 'marabiiiiiiiiiiiiii',
+        description:
+          'O atendimento não está habilitado para essa instância, mensagens nessa conexão não irão iniciar conversas no sistema de atendimento.',
+      }"
+    />
+  </div>
 </template>
 
 <script setup>
