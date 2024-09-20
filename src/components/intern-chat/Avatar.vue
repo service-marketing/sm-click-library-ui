5d4f9bce0d862d1f2fa58c8812d1e9d55f3a5888
JoaoDevSM <133679372+JoaoDevSM@users.noreply.github.com>
2024-09-20 17:56:06 -0300

    avatar shadow

--- src/components/intern-chat/Avatar.vue
+++ src/components/intern-chat/Avatar.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="avatar bg-[#404857] shadow shadow-gray-900 dark:shadow-gray-400 dark:bg-gray-300 text-gray-100 dark:text-gray-500">
+        class="avatar bg-[#404857] shadow shadow-gray-900 dark:shadow-gray-700 dark:bg-gray-300 text-gray-100 dark:text-gray-500">
         <svg style="padding-top: 0.5rem" v-if="!url" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
             stroke-width="1.5" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round"
