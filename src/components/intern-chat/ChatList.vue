99d2375a71cd38ac33b52eedbc59f70d10cc2b70
JoaoDevSM <133679372+JoaoDevSM@users.noreply.github.com>
2024-10-03 14:01:53 -0300

    test classes

--- src/components/intern-chat/ChatList.vue
+++ src/components/intern-chat/ChatList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container-chats">
+  <div class="container">
     <!-- Campo de busca -->
     <input v-model="searchQuery" class="search-input bg-base-300 border-b border-base-200" placeholder="Pesquise entre os atendentes..." />
 
@@ -95,7 +95,7 @@ const filteredAtendentes = computed(() => {
   }
 }
 
-.container-chats {
+.container {
   display: flex;
   flex-direction: column;
   height: 100%;
