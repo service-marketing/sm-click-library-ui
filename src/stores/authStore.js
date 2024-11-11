2db3df1dc6204f8da015dced31867ad99982b511
JoaoDevSM <133679372+JoaoDevSM@users.noreply.github.com>
2024-11-10 21:18:40 -0300

    update: token

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia';
 
 export const useAuthStore = defineStore('auth', {
     state: () => ({
-        jwtToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzMwMjk0NDE5LCJpYXQiOjE3MzAyMDgwMTksImp0aSI6IjNiMWRkOTA4N2RlNzQwNTY4MTg5NDljYmI5MDgwZmUzIiwidXNlcl9pZCI6IjU1ZTBhMDY0LTE2Y2UtNGM4Ny1hOWFjLTFmNDZmNGY1ZDI1ZCJ9.f54-6o6Q7G9KWfdnjoRck3sevfaS-E1t8r0cIAiNiZI' // Inicialmente vazio
+        jwtToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzMxMzcwNTU3LCJpYXQiOjE3MzEyODQxNTcsImp0aSI6IjYxNzQxM2VhZjU1ZDQyMjU4Njk3ZjIyN2VkNjIwNWM2IiwidXNlcl9pZCI6IjgzZDUxYzQ0LTY0OWEtNGUzYi1hYTUyLWRiMGQ4NjRiMTcxMSJ9.Na0dGJNNnqyZxLuMcGev7S07ULHCq-CNevApfzwXYmw' // Inicialmente vazio
     }),
     actions: {
         setToken(token) {
