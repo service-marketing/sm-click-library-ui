87c5e77558bbd299b893e5bf26fc3124fe324aca
matheusbanqueirosm <matheus.lima@grupo-sm-solucoes.com>
2025-09-15 09:36:28 -0300

    style: adjust finally modal

--- src/components/modals/simple_modal/simple_modal.vue
+++ src/components/modals/simple_modal/simple_modal.vue
@@ -28,44 +28,68 @@ const toggleModal = () => {
 </script>
 
 <template>
-  <div v-if="isOpen" class="modal_head">
-    <div class="modal-overlay absolute h-full w-full bg-gray-600 opacity-50" />
-    <div class="modal_responsive">
+  <Transition
+    leave-active-class="transition-opacity duration-200 ease-in"
+    leave-from-class="opacity-100"
+    leave-to-class="opacity-0"
+  >
+    <div v-if="isOpen" class="modal_head">
       <div
-        class="flex min-h-full items-center justify-center text-center sm:p-0"
-      >
-        <div class="z-50 w-full shadow rounded-2xl shadow-black" :class="size">
-          <!-- Modal content -->
-          <div class="modal_tittle text-current">
-            <section class="flex items-center gap-3">
-              <div v-html="header?.svg"></div>
+        class="modal-overlay absolute h-full w-full bg-gray-600 opacity-50"
+      />
+      <div class="modal_responsive">
+        <div
+          class="flex min-h-full items-center justify-center text-center sm:p-0"
+        >
+          <div
+            class="z-50 w-full shadow rounded-2xl shadow-black"
+            :class="size"
+          >
+            <!-- Modal content -->
+            <div class="modal_tittle text-current">
+              <section class="flex items-center gap-3">
+                <div v-html="header?.svg"></div>
 
-              <div>
-                <h3 class="text-xl font-semibold flex items-center gap-2">
-                  <p>{{ header?.title }}</p>
-                </h3>
                 <div>
-                  <slot name="header" />
+                  <h3 class="text-xl font-semibold flex items-center gap-2">
+                    <p>{{ header?.title }}</p>
+                  </h3>
+                  <div>
+                    <slot name="header" />
+                  </div>
                 </div>
-              </div>
-            </section>
+              </section>
 
-            <button @click="toggleModal()" class="modal_close_button">✖</button>
-          </div>
-
-          <div class="bg-base-200">
-            <div class="text-center">
-              <slot name="body" />
+              <button @click="toggleModal()" class="modal_close_button">
+                <svg
+                  class="h-5 w-5"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </button>
             </div>
-          </div>
 
-          <div class="modal_end_button">
-            <slot name="footer" />
+            <div class="bg-base-200">
+              <div class="text-center">
+                <slot name="body" />
+              </div>
+            </div>
+
+            <div class="modal_end_button">
+              <slot name="footer" />
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
+  </Transition>
 </template>
 
 <style scoped>
